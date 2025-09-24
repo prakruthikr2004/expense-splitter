d7534f1b2fb0a2dd4ff05e3d314be2105b706195
prakruthikr <prakruthikr2004@gmail.com>
2025-09-24 11:41:07 +0530

    fixed errors

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import './theme.css';
-import './index.css';
+import "./index.css";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Home from "./pages/Home.jsx";
 import App from "./App.jsx";
