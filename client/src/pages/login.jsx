a39287dcc1d4c48e483c4b5b078e203b7c34a0db
prakruthikr <prakruthikr2004@gmail.com>
2025-10-10 15:39:43 +0530

    checking

--- client/src/pages/login.jsx
+++ client/src/pages/login.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { toast, ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-
+//check for commits
 export default function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
